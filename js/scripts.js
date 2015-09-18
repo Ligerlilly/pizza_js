8b22534ba8857fdfda2f3574ea5fc5f6bf0d2742
Jason <quizathaderat@yahoo.com>
2015-09-18 15:21:06 -0700

    Add frontend

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ function Pizza() {
   };
 
   addTopping = function(topping) {
-    cost += topping.cost;
+    cost = cost + topping.cost;
   };
 
   return { size       : pieSize,
@@ -38,3 +38,44 @@ function Topping(name, cost) {
   this.name = name;
   this.cost = cost;
 }
+
+$(document).ready(function() {
+  $('form#pizza').submit(function(event) {
+    event.preventDefault();
+    p = new Pizza();
+    var toppingNames = [];
+    var toppingCosts = [];
+    var pieSize = $('select#pieSize').val();
+    var numPizzas = parseInt($('input#numPizzas').val());
+    p.size(pieSize);
+    p.quantity(numPizzas);
+    $('.toppingName').each(function() {
+      toppingNames.push($(this).text());
+    });
+
+    $('.toppingCost').each(function() {
+      toppingCosts.push($(this));
+    });
+
+    for (var i = 0; i < toppingCosts.length; i++) {
+      if (toppingCosts[i][0].checked) {
+        p.addTopping(new Topping(toppingNames[i], parseInt(toppingCosts[i].val())));
+      }
+
+    }
+    $('.results').show();
+    $('.total').text(p.pieCost());
+
+
+    // if (toppingCostMushrooms[0].checked) {
+    //   var toppingNameMushrooms = $('label#toppingNameMushrooms').text();
+    //   p.addTopping(new Topping(toppingNameMushrooms, parseInt(toppingCostMushrooms.val())));
+    // }
+    // alert(p.pieCost());
+
+
+
+
+
+  });
+});
