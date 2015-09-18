9377ed2a9eafc6201483938729376a509ae52814
Jason <quizathaderat@yahoo.com>
2015-09-18 12:36:34 -0700

    Add pizza returns cost based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-function Pizza() {
-  return true;
+function Pizza(size) {
+  if (size === 'small') {
+    return 5;
+  }
+  else if (size === 'medium'){
+    return 10;
+  }
+  else if (size === 'large') {
+    return 15;
+  }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,7 @@
 describe('Pizza', function() {
-  it('returns true', function() {
-    expect(Pizza()).to.eq(true);
+  it('returns cost based on size', function() {
+    expect(Pizza('small')).to.eq(5);
+    expect(Pizza('medium')).to.eq(10);
+    expect(Pizza('large')).to.eq(15);
   });
 });
